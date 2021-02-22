8fb773ac8b8c679642fb25fadec571a6bd39bdb2
vanevrb <vane.vrb@gmail.com>
2021-02-22 12:11:07 -0500

    ViewChild home

--- src/app/home/components/layout/layout.component.ts
+++ src/app/home/components/layout/layout.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild ,AfterViewInit,ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent implements OnInit,AfterViewInit {
+@ViewChild('mi') myDiv:ElementRef;
+
 
   constructor() { }
-
+  ngAfterViewInit() {
+    // this.myDiv.nativeElement.value ="sa";
+   console.log(this.myDiv.nativeElement) ;
+  }
   ngOnInit(): void {
+    
   }
 
 }
