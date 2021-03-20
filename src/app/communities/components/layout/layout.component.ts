6a271a2c06d5d9d306e2ee1a73112bdd8182ecb0
vanevrb <vane.vrb@gmail.com>
2021-03-20 17:30:08 -0500

    comu and workshops

--- src/app/communities/components/layout/layout.component.ts
+++ src/app/communities/components/layout/layout.component.ts
@@ -12,6 +12,77 @@ import { take } from 'rxjs/operators';
 export class LayoutComponent implements OnInit, OnDestroy {
   userStoreSub$: Subscription;
 
+  communities: any[] = [
+    {
+      id:1,
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'Los crypto',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }, {
+      id:2,
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }, {
+      id:3,
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'Lohttps://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }, {
+      id:4,
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }, {
+      id:5,
+
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }, {
+      id:6,
+
+      image:
+        'https://cdn.discordapp.com/avatars/785683857487036467/bc9ee60333180bee3e04afd2ed50bdac.png',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:'https://discord.gg/e9FEwCcZ9S'
+
+
+
+    }
+  ];
   constructor(private store: Store<AppStateWithUsers>) {}
 
   ngOnInit(): void {
--- src/app/learning/learning/learning.component.ts
+++ src/app/learning/learning/learning.component.ts
@@ -15,6 +15,77 @@ import { AppStateWithUsers } from '../../store/reducers/index';
 export class LearningComponent implements OnInit, OnDestroy {
   userStoreSub$: Subscription;
 
+  workshops: any[] = [
+    {
+      id:1,
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'Los crypto',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }, {
+      id:2,
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }, {
+      id:3,
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'Lohttps://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }, {
+      id:4,
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }, {
+      id:5,
+
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }, {
+      id:6,
+
+      image:
+        'http://localhost:4200/assets/images/event.svg',
+        link_share: 'https://www.comfeco.com/',
+        link_info:'https://www.comfeco.com/',
+        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+        link_unirse:''
+
+
+
+    }
+  ];
   constructor(private store: Store<AppStateWithUsers>) {}
 
   ngOnInit(): void {
